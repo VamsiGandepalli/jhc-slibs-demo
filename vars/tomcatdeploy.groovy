26d7aa9f46cde9b6beba03b12a9a93c26bf2ab4e
Vamsi <154397114+VamsiGandepalli@users.noreply.github.com>
2024-02-28 11:38:38 +0530

    Update and rename tomcatdeploy.groovy to tomcatDeploy.groovy

--- vars/tomcatdeploy.groovy
+++
@@ -1,7 +0,0 @@
-def call(tomcatIp, userName, credId, warName) {
-  sshagent([credid]) {
-      sh "scp -o StrictHostKeyChecking=no target/${warName} ${userName}@${tomcatIp}:/opt/tomcat9/webapps/"
-      sh "ssh ${userName}@${tomcatIp} /opt/tomcat9/bin/shutdown.sh"
-      sh "ssh ${userName}@${tomcatIp} /opt/tomcat9/bin/startup.sh"
-  }
-}
